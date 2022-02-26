cfeacca17fac90507c89f0db78ad5b45437b0356
Daniel Españadero <developer@daniel-espanadero.com>
2022-02-26 22:43:10 +0100

    Server created

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import express from 'express';
+import paymentRouter from './routes/payment.routes';
 
+const app = express();
+
+app.use(paymentRouter);
+
+app.listen(3000);
+console.log('server on port', 3000);
