b8039e37b27c2e3a16f0d73cad86c142220498e7
Daniel Españadero <developer@daniel-espanadero.com>
2022-02-26 23:25:11 +0100

    Paypal routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import paymentRouter from './routes/payment.routes';
+import morgan from 'morgan';
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(paymentRouter);
 
 app.listen(3000);
--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -2,8 +2,16 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello Word');
+router.get('/create-order', (req, res) => {
+    res.send('Creating an order');
+});
+
+router.get('/capture-order', (req, res) => {
+    res.send('Capturing order');
+});
+
+router.get('/cancel-order', (req, res) => {
+    res.send('Canceling order');
 });
 
 export default router;
